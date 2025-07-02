6ff9dfd4cb90660c3c930627d600c1f542408c84
Stephane Bouchet <sbouchet@redhat.com>
2025-07-02 18:02:12 +0200

    chore: cleanup
    Signed-off-by: Stephane Bouchet <sbouchet@redhat.com>

---
+++ src/main/java/com/redhat/devtools/intellij/commonuitest/utils/build/BuildUtils.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution,
+ * and is available at https://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ * Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package com.redhat.devtools.intellij.commonuitest.utils.build;
+
+import com.intellij.remoterobot.RemoteRobot;
+import com.redhat.devtools.intellij.commonuitest.exceptions.UITestException;
+import com.redhat.devtools.intellij.commonuitest.fixtures.mainidewindow.idestatusbar.IdeStatusBar;
+import com.redhat.devtools.intellij.commonuitest.fixtures.mainidewindow.toolwindowspane.BuildView;
+import com.redhat.devtools.intellij.commonuitest.fixtures.mainidewindow.toolwindowspane.ToolWindowPane;
+import com.redhat.devtools.intellij.commonuitest.fixtures.mainidewindow.toolwindowspane.buildtoolpane.MavenBuildToolPane;
+
+import java.time.Duration;
+
+/**
+ * Build project utility class
+ */
+public final class BuildUtils {
+
+    private BuildUtils() {throw new UITestException("Utility class with static methods.");}
+
+    /**
+     * Build a Maven project and wait for the result to be successful
+     *
+     * @param remoteRobot reference to the RemoteRobot instance
+     * @param projectName the name of the project
+     * @param goal        the maven goal
+     */
+    public static void buildMavenProjectAndWaitForFinish(RemoteRobot remoteRobot, String projectName, String goal) {
+        ToolWindowPane toolWinPane = remoteRobot.find(ToolWindowPane.class, Duration.ofSeconds(10));
+        toolWinPane.openMavenBuildToolPane();
+        MavenBuildToolPane mavenBuildToolPane = toolWinPane.find(MavenBuildToolPane.class, Duration.ofSeconds(10));
+        mavenBuildToolPane.buildProject(goal, projectName);
+        toolWinPane.find(BuildView.class, Duration.ofSeconds(5)).waitUntilBuildHasFinished();
+        remoteRobot.find(IdeStatusBar.class, Duration.ofSeconds(30)).waitUntilAllBgTasksFinish();
+    }
+}
